d73cf9bc29e53b6deef228ac4245124f3255ab59
GumirYo <agafurov.gumir@gmail.com>
2026-04-30 10:07:07 +0300

    Update game.js

--- js/game.js
+++ js/game.js
@@ -60,7 +60,7 @@ export class Game {
         this.owl = new OwlNPC(this);                     
         const groundY = this.canvas.height - 100;        
         const owlHeight = this.owl.sprite.frameHeight * this.owl.sprite.scale; 
-        this.owl.setPosition(groundY - owlHeight);  
+        this.owl.setPosition(200, groundY);  
 
         // Load assets
         this.initBackgrounds();
